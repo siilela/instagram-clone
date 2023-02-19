d02acdbf3efeb352299cc1fb42b14c2a18b537fa
Siiela Mogashoa <sumogashoa@shoprite.co.za>
2023-02-19 18:20:11 +0200

    UI and Auth using firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import { db, auth } from "./firebase";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [posts, setPost] = useState([]);
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+        if (authUser.displayName) {
+          //Dont update user name
+        } else {
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      } else {
+        setUsername(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [user, username]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPost(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
+  const signUp = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <Input
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signUp}>Sign Up</Button>
+            </center>
+          </form>
+        </div>
+      </Modal>
+
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.mediaweek.com.au/wp-content/uploads/2022/10/Instagram-logo.jpg"
+          alt=""
+        />
+      </div>
+      {/* Posts */}
+
+      {user ? (
+        <Button onClick={() =>auth.signOut()}>Logout</Button>
+      ) : (
+        <Button onClick={() => setOpen(true)}>Sign up</Button>
+      )}
+
+      {posts.map(({ id, userName, caption, imageURL }) => (
+        <Post
+          key={id}
+          userName={userName}
+          caption={caption}
+          imageURL={imageURL}
+        />
+      ))}
+
+      {/* Posts */}
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
+
+const Post = ({ userName, caption, imageURL }) => {
+  return (
+    <div className="post">
+      <div className="post__header">
+        {/* header avatar username */}
+        <Avatar
+          className="post__avatar"
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h1>{userName}</h1>
+      </div>
+      {/* Images */}
+      <img className="post__image" src={imageURL} alt="" />
+
+      {/* Username Caption */}
+      <h4 className="post__text">
+        <strong>{userName}</strong> {caption}
+      </h4>
+    </div>
+  );
+};
+
+export default Post;
